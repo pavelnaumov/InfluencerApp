50d72e326c41f0fe50bf725f74d942af9285d594
Pavel Naumov <pavliknaumov@gmail.com>
2018-09-17 17:07:37 +0100

    decided to stick to one plan + done seeds

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,73 +1,89 @@
-Influencer.destroy_all
-Job.destroy_all
-Client.destroy_all
+User.destroy_all
 
 puts "Finished cleaning"
 
-Influencer.create!(
+User.create!(
   username: "jack",
   photo: "https://randomuser.me/api/portraits/men/29.jpg",
-  NIF: "1234567",
+  nif: "1234567",
   location: "Lisbon",
   age: 23,
   influence_level: "High",
   global_score: 100,
-  gender: "male"
+  gender: "male",
+  email:' jack@mail.com',
+  password: 123456,
+  role: 0
   )
 
-Influencer.create!(
+User.create!(
   username: "john",
   photo: "https://randomuser.me/api/portraits/men/26.jpg",
-  NIF: "1234567",
+  nif: "1234567",
   location: "Porto",
   age: 24,
   influence_level: "High",
   global_score: 120,
-  gender: "male"
+  gender: "male",
+  email:' john@mail.com',
+  password: 123456,
+  role: 0
   )
 
-Influencer.create!(
+User.create!(
   username: "tom",
   photo: "https://randomuser.me/api/portraits/men/30.jpg",
-  NIF: "1234567",
+  nif: "1234567",
   location: "Coimbra",
   age: 26,
   influence_level: "High",
   global_score: 110,
-  gender: "male"
+  gender: "male",
+  email:' tom@mail.com',
+  password: 123456,
+  role: 0
   )
 
-Influencer.create!(
+User.create!(
   username: "kate",
   photo: "https://randomuser.me/api/portraits/women/68.jpg",
-  NIF: "1234567",
+  nif: "1234567",
   location: "Lisbon",
   age: 22,
   influence_level: "High",
   global_score: 100,
-  gender: "female"
+  gender: "female",
+  email:' kate@mail.com',
+  password: 123456,
+  role: 0
   )
 
-Influencer.create!(
+User.create!(
   username: "maria",
   photo: "https://randomuser.me/api/portraits/women/66.jpg",
-  NIF: "1234567",
+  nif: "1234567",
   location: "Porto",
   age: 23,
   influence_level: "High",
   global_score: 110,
-  gender: "female"
+  gender: "female",
+  email:' maria@mail.com',
+  password: 123456,
+  role: 0
   )
 
-Influencer.create!(
+User.create!(
   username: "rita",
   photo: "https://randomuser.me/api/portraits/women/69.jpg",
-  NIF: "1234567",
+  nif: "1234567",
   location: "Coimbra",
   age: 24,
   influence_level: "High",
   global_score: 120,
-  gender: "female"
+  gender: "female",
+  email:' rita@mail.com',
+  password: 123456,
+  role: 0
   )
 
 puts "finished creating influencers" 
@@ -75,19 +91,19 @@ puts "creating categories"
 
 
 Category.create!(
-  name: "Fashion",
+  category_name: "Fashion",
   description: "High fashion, make-up, etc",
   picture: "https://fashionista.com/.image/t_share/MTUyMzgxNDYxOTg1NTAyNzA0/influencers-to-watch-2018.jpg",
   )
 
 Category.create!(
-  name: "Cooking",
+  category_name: "Cooking",
   description: "Nice food and drinks",
   picture: "https://cdn-images-1.medium.com/max/1600/1*b7dvok_6-JsrlVLctnhctg.jpeg",
   )
 
 Category.create!(
-  name: "Sports",
+  category_name: "Sports",
   description: "Sports, fintess, wellness",
   picture: "http://www.fuelcommunications.com.au/wp-content/uploads/2016/10/CARD_04_573.jpg",
   )
@@ -97,48 +113,66 @@ puts "Finished creating categories"
 
 puts "creating clients"
 
-Client.create!(
+User.create!(
   username: "valerio",
-  NIF: "223344",
+  nif: "223344",
   location: "Lisbon",
-  company: "Nice Company Co."
+  company: "Nice Company Co.",
+  email:' valerio@mail.com',
+  password: 123456,
+  role: 1
   )
 
-Client.create!(
+User.create!(
   username: "joao",
-  NIF: "253344",
+  nif: "253344",
   location: "Porto",
-  company: "Super Company Co."
+  company: "Super Company Co.",
+  email:' joao@mail.com',
+  password: 123456,
+  role: 1
   )
 
-Client.create!(
-  username: "Paulo",
-  NIF: "235344",
+User.create!(
+  username: "paulo",
+  nif: "235344",
   location: "Coimbra",
-  company: "Awesome Company Co."
+  company: "Awesome Company Co.",
+  email:' paulo@mail.com',
+  password: 123456,
+  role: 1
   )
 
-Client.create!(
+User.create!(
   username: "joana",
-  NIF: "263344",
+  nif: "263344",
   location: "Lisbon",
-  company: "Amazing Company Co."
+  company: "Amazing Company Co.",
+  email:' joana@mail.com',
+  password: 123456,
+  role: 1
   )
 
 
-Client.create!(
+User.create!(
   username: "monica",
-  NIF: "264362",
+  nif: "264362",
   location: "Porto",
-  company: "Clever Company Co."
+  company: "Clever Company Co.",
+  email:' monica@mail.com',
+  password: 123456,
+  role: 1
   )
 
 
-Client.create!(
+User.create!(
   username: "shannon",
-  NIF: "663321",
+  nif: "663321",
   location: "Coimbra",
-  company: "Great Company Co."
+  company: "Great Company Co.",
+  email:' shannon@mail.com',
+  password: 123456,
+  role: 1
   )
 
 
